c26a60f5848738a9e0bb8029a7ce091801357323
style198708 <style1987@sina.cn>
2016-06-28 17:13:42 +0800

    Async

---
+++ AsyncWait/AppAsync.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncWait
+{
+    public class AppAsync
+    {
+        public AppAsync()
+        {
+            Console.WriteLine("MyClass() End."); 
+        }
+        /// <summary>
+        /// 加法
+        /// </summary>
+        /// <returns></returns>
+        public Task<double> GetValueAsync(double num1, double num2)
+        {
+            return Task.Run<double>(() =>
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    num1 = num1 + num2;
+                }
+                return num1;
+            });
+        }
+
+        /// <summary>
+        /// 异步方法(例子)
+        /// </summary>
+        public async Task<double> DisplayValue(double num1, double num2)
+        {
+            return await Task<double>.Run<double>(() =>
+             {
+                 for (int i = 0; i < 1000000; i++)
+                 {
+                     num1 = num1 + num2;
+                 }
+                 return num1;
+             });
+        }
+    }
+}
